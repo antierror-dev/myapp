c0f474aaf579632646029dc8f0d74e42ea76ae46
MmdAli <core.my.activity@gmail.com>
2024-01-09 11:09:33 +0330

    new: set env file and configuration

--- configs/config.go
+++ configs/config.go
@@ -3,25 +3,44 @@ package configs
 import (
   "github.com/gofiber/fiber/v2"
   "github.com/goccy/go-json"
+  "github.com/joho/godotenv"
+  "os"
   "fmt"
+  "strconv"
   //"github.com/gofiber/template/html/v2"
   )
 
 
+func parsebool(str string) (bool) {
+  res,_ := strconv.ParseBool(str)
+  return res
+}
 
-var(
-  APP_NAME string = "MyApp"
-  SERVER_HEADER string  = "Petros"
-  MULTI_CPU bool = true
+
+
+var (
+APP_NAME string
+SERVER_HEADER string
+MULTI_CORE bool
 )
+
+func loadenv(){
+  err := godotenv.Load(".env")
+if err!=nil{panic("set .env file")}
+  APP_NAME = os.Getenv("APP_NAME")
+  SERVER_HEADER = os.Getenv("SERVER_HEADER")
+  MULTI_CORE = parsebool(os.Getenv("MULTI_CORE"))
+}
+
 
 var Appconfig fiber.Config
 func init(){
+  loadenv()
 //engine := html.New("./views", ".html")
 Appconfig = fiber.Config{
     AppName: APP_NAME,
     ServerHeader: SERVER_HEADER,
-    Prefork: MULTI_CPU,
+    Prefork: MULTI_CORE,
     CaseSensitive: false,
     Concurrency: 256 * 1024,//connected devices len
     DisableStartupMessage: false,
