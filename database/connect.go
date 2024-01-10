f032343551cd1e8c43ef13860ff505badd02f8b6
MmdAli <core.my.activity@gmail.com>
2024-01-10 09:38:36 +0330

    new: add database configuration

---
+++ database/connect.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+  "github.com/glebarez/sqlite"
+  "gorm.io/gorm"
+  "fmt"
+  //"myapp/models"
+)
+
+var db *gorm.DB
+
+func init() {
+  var err error
+  db, err = gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
+  if err != nil || db == nil {
+    panic("failed to connect database")
+  }
+  fmt.Println("connected to db")
+  //db.AutoMigrate(&models.User{})
+  fmt.Println("fields of db is created")
+}
+
+
+func GetDB() *gorm.DB {
+  return db
+}
